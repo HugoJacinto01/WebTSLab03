46545fcf68c46238b675d51a152cf4d8ccd32b6b
Hugo Jacinto <hugo_jg10@hotmail.com>
2025-06-13 18:42:35 -0600

    Avance de tipo de datos

--- TypeScripts/Init.ts
+++ TypeScripts/Init.ts
@@ -1,5 +1,13 @@
 (() => {
     let a: string = "Hugo Daniel";
     let b: string = `Hola: ${a}`;
+    let multiArray: (string | number)[] = ['Hugo', 1, 'Daniel', 2, 'Gonzalez', 3];
+    let multiTupla: [string, number, string, number, string, number] = ['Hugo', 1, 'Daniel', 2, 'Gonzalez', 3];
+    enum Color { Red, Green, Blue };
+    let VarNull: undefined = undefined;
     console.log(b[15]?.toUpperCase() || 'No existe');
+    console.log(multiArray[2]);
+    console.log(multiTupla[3]);
+    console.log(Color.Green);
+    console.log(VarNull);
 })();
--- TypeScripts/init.js
+++ TypeScripts/init.js
@@ -3,5 +3,19 @@
     var _a;
     let a = "Hugo Daniel";
     let b = `Hola: ${a}`;
+    let multiArray = ['Hugo', 1, 'Daniel', 2, 'Gonzalez', 3];
+    let multiTupla = ['Hugo', 1, 'Daniel', 2, 'Gonzalez', 3];
+    let Color;
+    (function (Color) {
+        Color[Color["Red"] = 0] = "Red";
+        Color[Color["Green"] = 1] = "Green";
+        Color[Color["Blue"] = 2] = "Blue";
+    })(Color || (Color = {}));
+    ;
+    let VarNull = undefined;
     console.log(((_a = b[15]) === null || _a === void 0 ? void 0 : _a.toUpperCase()) || 'No existe');
+    console.log(multiArray[2]);
+    console.log(multiTupla[3]);
+    console.log(Color.Green);
+    console.log(VarNull);
 })();
